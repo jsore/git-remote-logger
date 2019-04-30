09859155bc4fc17cf83643636df857f97eccae51
Justin Sorensen <jus.a.sorensen@gmail.com>
2019-04-29 23:31:07 -0500

    Psuedo'ed what to do when a command is fired

--- new-remote.js
+++ new-remote.js
@@ -37,7 +37,9 @@ const _ = require('lodash');
 
 // $ git remote add origin git@github.com-jsore:jsore/git-remote-logger.git
 
-const defaultBranch = 'origin';
+const origin = 'origin';
+
+/** command parameters */
 const sshHostArg = {
     describe: 'Which SSH Host to use ( see ~/.ssh/config for options )',
     demand: true,
@@ -48,13 +50,22 @@ const remoteArg = {
     demand: true,
     alias: 'r'
 };
+const branchArg = {
+    describe: 'Optional branch to push staged files to',
+    demand: false,
+    alias: 'b'
+}
 
+/** what to do with those parameters */
 const argv = yargs
     .command('add', 'Add a new remote', {
         host: sshHostArg,
         remote: remoteArg
     })
     .command('view-all', 'View all connected SSH remotes')
+    .command('push', 'Push staged files to the remote branch', {
+        branch: branchArg
+    })
     /** make available at comman runtime */
     .help()
     /** call argv on return of command() for yarg to store its args */
@@ -65,6 +76,63 @@ var command = argv._[0];
 // console.log('Command: ', command);
 // console.log('raw process.argv version: ', process.argv);
 // console.log('raw lodash + yargs version:', argv);
+
+
+// from NodeBasics/notesApp-node/app.js
+if (command === 'add') {
+    // add a remote and log the addition
+
+    var note = notes.addNote(argv.title, argv.body);
+    if (note) { notes.logNoteSuccess(note, 'added successfuly');
+    } else { notes.logNoteAlt('Note title taken'); }
+
+} else if (command === 'view-all') {
+    // view all remotes logged with add
+
+    var allNotes = notes.getAll();
+    var allBoolean = true;
+    allNotes.forEach((note) => notes.logNoteSucces(note, 'retrieved', allBoolean));
+
+} else if (command === 'push') {
+    // push commits to origin <branch>
+
+    var noteRemoved = notes.removeNote(argv.title);
+    var message = noteRemoved ? 'Note removed' : 'Note not found';
+    notes.logNoteAlt(message);
+
+} else { notes.logNoteAlt(`COmmand \"${command}\" not recognized`); }
+
+
+
+
+/**
+ * lodash utility isString
+ * -- returns true if thing is a string
+ * -- returns false if thing isn't a string
+ */
+//console.log(_.isString(true));  // false
+//console.log(_.isString('gary'));  // true
+
+/**
+ * lodash utility uniq
+ * -- take an array, return it with duplicates removed
+ */
+//var filteredArray = _.uniq(['name', 1, 'name', 1, 2, 3, 4]);
+//var filteredArray = _.uniq(['Mike']);
+//console.log(filteredArray);
+
+
+
+/** basic usage of appendFile(): */
+//fs.appendFile('greetings.txt', 'Hello World!');  // Hello World!
+/** concat'ing a variable: */
+//fs.appendFile('greetings.txt', 'Hello' + user.username +'!');  // Helloroot!
+/** using ES6 template strings (`string ${variable} more string`): */
+//fs.appendFile('greetings.txt', `Hello ${user.username}!`);  // Hello root!
+/** template strings and usage of exports from note.js: */
+//fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);  // Hello root! You are 25.
+
+
 
 
 //if (!logfile) {
