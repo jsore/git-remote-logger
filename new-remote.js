365d3106930b70e139bad417e59b0008cc1e2c72
Justin Sorensen <jus.a.sorensen@gmail.com>
2019-05-01 23:24:05 -0500

    Add function to store remotes

--- new-remote.js
+++ new-remote.js
@@ -28,8 +28,7 @@
 // notesApp-node/app.js
 
 const fs = require('fs');
-const remotesLog = './git-remotes.txt';
-const trigger = './new-remote.txt';
+//const remotesLog = './git-remotes.txt';
 const yargs = require('yargs');
 
 //const dir = require('node-dir');
@@ -38,114 +37,67 @@ const parse = require('./lib/parser.js');
 
 
 const _ = require('lodash');
-    /**
-     * - Iterating arrays, objects, & strings
-     * - Manipulating & testing values
-     * - Creating composite functions
-     */
+  /**
+   * - Iterating arrays, objects, & strings
+   * - Manipulating & testing values
+   * - Creating composite functions
+   */
 
 const path = require('path');
-const cwd = process.cwd();
+const source = process.cwd();
 
 /** what to do with those parameters */
 
+
+
 const argv = yargs
-    .command('add', 'Add a new remote', {
-        remote: {
-            alias: 'r',
-            describe: 'Remote URL or SSH address to use',
-            demandOption: true,
-        },
-        host: {
-            alias: 'h',
-            describe: 'Optional custom SSH Host to use'
-        },
-        origin: {
-            alias: 'o',
-            describe: 'A name for the remote repository',
-            default: 'origin'
-        },
-    }, (argv) => {
-        const host = argv.host;
-        const remote = argv.remote;
-        const origin = argv.origin;
-        //const command = argv._[0];
-        const command = argv._[0];
+  .command('add', 'Add a new remote', {
+    r: {
+      alias: 'remote',
+      describe: 'Remote URL or SSH address to use',
+      demandOption: true,
+    },
+    h: {
+      alias: 'host',
+      describe: 'Optional custom SSH Host to use'
+    },
+    o: {
+      alias: 'origin',
+      describe: 'Specify a reference to this repo',
+      default: 'origin'
+    },
+    b: {
+      alias: 'branch',
+      describe: 'Specify a parent branch for this repo',
+      default: 'master'
+    }
+  })
+  .command('view-all', 'View all connected SSH remotes', (argv) => {
+    parse.remotes();
+  })
+  //.command('pull', 'Grab an files for an existing project')
+  //.command('remove', 'Remove a remote')
+  .command('push', 'Push staged files to the remote branch', {
+    //branch: branchArg
+  })
+  .demandCommand(1, 'Use --help options for guidance')
+  .help('help', 'Show usage information')
+  .example('$0 --help', 'General usage')
+  .example('$0 add --help', 'Command-specific usage')
+  // .coerce(['src', 'dest'], path.resolve)
+  /** call argv on return of command() for yarg to store its args */
+  .argv;
 
-        console.log(
-            `You hit '${command}' from: ${cwd}\n `,
-            `host:      ${host}\n `,
-            `remote:    ${remote}\n `,
-            `origin:    ${origin}\n `,
-            ``);
+const command = argv._[0];
+// if (command === 'add') { parse.addition(argv) }
+console.log(argv);
+if (command === 'add') {
+  //const logfile = fs.readFileSync('notes-data.json');
+  let remote = parse.addition(argv, source);
+  if (remote) { console.log(remote, 'added succesfully'); }
+  //else {  }
+}
 
-        let type = argv.remote.split(':');
-        let str = type[0].toString();
-
-        if (argv.host) {  // ssh with custom remote
-
-            //parse.addition(str, type);
-            parse.addition({str, type});
-
-        } else if (str === 'https') {  // https remote
-
-            console.log(`HTTPS remote: ${remote}`);
-
-        } else {  // ssh without custom host
-
-            console.log(`SSH remote: ${remote}`);
-
-        }
-    })
-    .command('view-all', 'View all connected SSH remotes', (argv) => {
-        parse.remotes();
-    })
-    //.command('pull', 'Grab an files for an existing project')
-    //.command('remove', 'Remove a remote')
-    .command('push', 'Push staged files to the remote branch', {
-        //branch: branchArg
-    })
-    .demandCommand(1, 'Use --help options for guidance')
-    .help('help', 'Show usage information')
-    .example('$0 --help', 'General usage')
-    .example('$0 add --help', 'Command-specific usage')
-    // .coerce(['src', 'dest'], path.resolve)
-    /** call argv on return of command() for yarg to store its args */
-    .argv;
-
-//if (command === 'view-all') {
-//    //(argv) => {
-//        const host = argv.host;
-//        const remote = argv.remote;
-//        const origin = argv.origin;
-//        //const command = argv._[0];
-//        const command = argv._[0];
-//
-//        console.log(
-//            `You hit '${command}' from: ${cwd}\n `,
-//            `host:      ${host}\n `,
-//            `remote:    ${remote}\n `,
-//            `origin:    ${origin}\n `,
-//            ``);
-//
-//        let type = argv.remote.split(':');
-//        let str = type[0].toString();
-//
-//        if (argv.host) {  // ssh with custom remote
-//
-//            //parse.addition(str, type);
-//            parse.addition({str, type});
-//
-//        } else if (str === 'https') {  // https remote
-//
-//            console.log(`HTTPS remote: ${remote}`);
-//
-//        } else {  // ssh without custom host
-//
-//            console.log(`SSH remote: ${remote}`);
-//
-//        }
-//};
 // $ git remote add origin git@github.com-jsore:jsore/git-remote-logger.git
 // github-remotes add -r https://github.com/jsore/git-remote-logger.git
 // github-remotes add -r git@github.com:jsore/git-remote-logger.git
@@ -156,41 +108,25 @@ const argv = yargs
 
 /** the command provided to program github-remotes */
 
-//console.log('Command: ', command);
-
-// console.log('raw process.argv version: ', process.argv);
-//console.log('raw lodash + yargs version:\n', argv);
-//console.log('source dir: ', cwd);
-
-
 
 // from NodeBasics/notesApp-node/app.js
 //if (command === 'add') {
-//    // add a remote and log the addition
 //
-//
-//    // console.log(`Adding ${remote} with SSH Host '${host}'`);
-//    // console.log(`Adding ${remoteArg} with SSH Host '${sshHost}'`);
-//    console.log(`Adding ${argv.remote} with SSH Host '${argv.host}'`);
-//
-//
-//    // var note = notes.addNote(argv.title, argv.body);
-//    // if (note) { notes.logNoteSuccess(note, 'added successfuly');
-//    // } else { notes.logNoteAlt('Note title taken'); }
+//  var note = notes.addNote(argv.title, argv.body);
+//  if (note) { notes.logNoteSuccess(note, 'added successfuly');
+//  } else { notes.logNoteAlt('Note title taken'); }
 //
 //} else if (command === 'view-all') {
-//    // view all remotes logged with add
 //
-//    // var allNotes = notes.getAll();
-//    // var allBoolean = true;
-//    // allNotes.forEach((note) => notes.logNoteSucces(note, 'retrieved', allBoolean));
+//  var allNotes = notes.getAll();
+//  var allBoolean = true;
+//  allNotes.forEach((note) => notes.logNoteSucces(note, 'retrieved', allBoolean));
 //
 //} else if (command === 'push') {
-//    // push commits to origin <branch>
 //
-//    // var noteRemoved = notes.removeNote(argv.title);
-//    // var message = noteRemoved ? 'Note removed' : 'Note not found';
-//    // notes.logNoteAlt(message);
+//  var noteRemoved = notes.removeNote(argv.title);
+//  var message = noteRemoved ? 'Note removed' : 'Note not found';
+//  notes.logNoteAlt(message);
 //
 //} else { notes.logNoteAlt(`COmmand \"${command}\" not recognized`); }
 
